e1973bc0436f380aa81279e754b0eacdac151035
Nizar Fergag <fergag99@gmail.com>
2022-03-16 13:03:36 +0100

    esercizio terminato

---
+++ Array/asset/js/main.js
@@ -0,0 +1,5 @@
+let array = [ 2, 3 , 4, 5, 6, 7, 7, 8, 9 ];
+
+for(let i = array.length - 5; i < array.length: i++) {
+    document.getElementById("container").innerHTML = `<div>${array[i]}</div>`;
+}
